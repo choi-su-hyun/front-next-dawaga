7870c342870d4c61bded2eb99572035741d39ccd
brooks-create <86158560+choi-su-hyun@users.noreply.github.com>
2025-02-17 21:14:03 +0900

    fix :: 모달에서는 폰트가 적용되지 않는 문제 수정

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,21 +1,23 @@
 import { ReactNode } from "react";
 import Header from "./common/Header/Header";
-import localFont from "next/font/local";
 
 interface Props {
   children: ReactNode;
 }
-const pretendard = localFont({
-  src: "../pages/fonts/PretendardVariable.woff2",
-  display: "swap",
-  weight: "45 920",
-  variable: "--font-pretendard",
-});
+
+// COMMENT : 모달까지 적용하기 위해 fontFace로 pretenderd를 적용하였습니다.
+// const pretendard = localFont({
+//   src: "../pages/fonts/PretendardVariable.woff2",
+//   display: "swap",
+//   weight: "45 920",
+//   variable: "--font-pretendard",
+// });
 
 export default function Layout({ children }: Props) {
   return (
     <>
-      <div className={`${pretendard.variable}`}>
+      {/* <div className={`${pretendard.variable}`}> */}
+      <div>
         <Header />
         <main>{children}</main>
       </div>
