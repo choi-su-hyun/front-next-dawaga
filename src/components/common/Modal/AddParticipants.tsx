d9b3d574cf0f8fc666b7d92475011f9a7ddb6b77
brooks-create <86158560+choi-su-hyun@users.noreply.github.com>
2025-02-17 21:15:03 +0900

    feat :: 모달 헤더 제작

--- src/components/common/Modal/AddParticipants.tsx
+++ src/components/common/Modal/AddParticipants.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import SearchInput from "../Input/SearchInput";
 import { useForm } from "react-hook-form";
+import Button from "../Button/Button";
+import AddIcon from "@/assets/add.svg";
+import style from "./Modal.module.scss";
+import NoticeIcon from "@/assets/notice.svg";
 
 const AddParticipants = () => {
   const { register } = useForm<{ participantsSearch: string }>({
@@ -15,7 +19,21 @@ const AddParticipants = () => {
 
   return (
     <>
-      <h3 className="modal__title">참여 인원 추가</h3>
+      <div className={style["modal__header"]}>
+        <h3 className={style["modal__title"]}>참여 인원 추가</h3>
+        <Button variant="text-cta-btn" leftSlot={<AddIcon />}>
+          비회원 초대
+        </Button>
+      </div>
+
+      <div className={style["modal__explanation-wrap"]}>
+        <NoticeIcon />
+        <span className={style["modal__explanation"]}>
+          아직 가입하지 않은 인원을 초대하시려면{" "}
+          <b className="bold">‘비회원 초대’</b>를 이용해주세요.
+        </span>
+      </div>
+
       <SearchInput
         register={register("participantsSearch")}
         name="participants-search"
